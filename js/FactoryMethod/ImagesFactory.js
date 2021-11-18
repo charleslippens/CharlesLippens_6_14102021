d6bc2d2a49a338be981e5855d494f16d235940d7
CL <cl@gmail.com>
2021-11-18 10:38:22 +0100

    correction alt images

--- js/FactoryMethod/ImagesFactory.js
+++ js/FactoryMethod/ImagesFactory.js
@@ -4,6 +4,8 @@ export default class Image {
 	// création de l'élement image
 	createHTML(element) {
 		const eltImage = document.createElement("img");
+		eltImage.setAttribute("alt", element.alt);
+		eltImage.setAttribute("role", "button");
 		eltImage.setAttribute("src", element.image);
 		eltImage.className = "ph-media";
 		return eltImage;
